4918ba26e46df3f9e2c843c5a359799afac48fe4
emanuelefirmani <emanuelefirmani@kpmg.com>
2024-12-01 18:39:01 +0100

    Project has some utility methods for dealing with text

---
+++ AdventOfCode/FileSplitter.fs
@@ -0,0 +1,17 @@
+﻿module AdventOfCode.FileSplitter
+
+open System
+
+let splitInLines (text: string) = text.Split('\n')
+let splitLine f (line: string) = line.Split(' ', StringSplitOptions.RemoveEmptyEntries) |> f
+
+let mapToTwoInts (items: string array) =
+    [items[0] |> int; items[1] |> int]
+
+let splitInLinesInTwoInts text =
+    splitInLines text
+    |> Array.map (splitLine mapToTwoInts)
+    |> List.ofArray
+
+let splitList (l: 'a list list) (col: int) : 'a list =
+    l |> List.map (fun r -> r[col])
