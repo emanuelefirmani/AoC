b0fc61e1d0e6b397fa81f0b0b6aa9b91d9355e9c
emanuelefirmani <emanuelefirmani@kpmg.com>
2024-12-01 18:12:22 +0100

    D01: can compute distance of lists

---
+++ AdventOfCode/D01/Resolution.fs
@@ -0,0 +1,15 @@
+﻿module AdventOfCode.D01.Resolution
+
+open System
+open Microsoft.FSharp.Core
+
+let computeItemDistance (i1: int, i2: int) =
+    i1 - i2 |> Math.Abs
+
+let rec computeDistance l1 l2 =
+    let l1Ordered = l1 |> Seq.sort
+    let l2Ordered = l2 |> Seq.sort
+
+    let distances = Seq.zip l1Ordered l2Ordered |> Seq.map computeItemDistance
+
+    distances |> Seq.sum
--- AdventOfCode/D01/Tests.fs
+++ AdventOfCode/D01/Tests.fs
@@ -1,8 +1,14 @@
 ﻿module Tests
 
-open System
+open AdventOfCode.D01
 open Xunit
+open Resolution
 
 [<Fact>]
 let ``My test`` () =
-    Assert.True(true)
+    let l1 = [3; 4; 2; 1; 3; 3]
+    let l2 = [4; 3; 5; 3; 9; 3]
+
+    let distance = computeDistance l1 l2
+
+    Assert.Equal(11, distance)
