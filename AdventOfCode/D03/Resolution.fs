174166e1a8c7a53851c89ecf17cfcbee3fcc9189
emanuelefirmani <emanuelefirmani@kpmg.com>
2024-12-04 19:44:17 +0100

    D03: commands are read from string via Regex

---
+++ AdventOfCode/D03/Resolution.fs
@@ -0,0 +1,13 @@
+﻿module AdventOfCode.D03.Resolution
+
+open System.Text.RegularExpressions
+
+let parseAndMultiply v1 v2 =
+    let d1 = v1 |> decimal
+    let d2 = v2 |> decimal
+    d1 * d2
+
+let computeMultiplications input =
+    Regex.Matches(input, "mul\((\d*),(\d*)\)")
+    |> Seq.map (fun m -> parseAndMultiply m.Groups[1].Value m.Groups[2].Value)
+    |> Seq.sum
