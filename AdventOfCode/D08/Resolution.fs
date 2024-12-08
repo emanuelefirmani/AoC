da279ec08de0e83b922f6171267698a5a09197ca
emanuelefirmani <emanuelefirmani@kpmg.com>
2024-12-08 11:33:01 +0100

    D08: parses input for antennas

---
+++ AdventOfCode/D08/Resolution.fs
@@ -0,0 +1,24 @@
+﻿module AdventOfCode.D08.Resolution
+
+open System.Text.RegularExpressions
+open AdventOfCode
+open FileSplitter
+
+type CoordY = int
+type CoordX = int
+type Coordinates = { x: CoordX; y: CoordY }
+type Antenna = { x: CoordX; y: CoordY; Frequency: string }
+
+let toCoordinates a = { x = a.x; y = a.y }
+
+let parseLine (y: CoordY) line =
+    let r = Regex("[a-zA-Z0-9]")
+
+    r.Matches line
+    |> Seq.map (fun m -> { x = m.Index; y = y; Frequency = m.Value })
+
+let parseInput input =
+    splitInLines input
+    |> Seq.mapi parseLine
+    |> Seq.collect id
+    |> Seq.groupBy (_.Frequency)
---
+++ AdventOfCode/D08/Tests.fs
@@ -0,0 +1,41 @@
+﻿module AdventOfCode.D08.Tests
+
+open AdventOfCode.D08.Resolution
+open Xunit
+open TestCases
+
+let antennaCoordinates input frequency =
+    let actual = parseInput input
+
+    actual
+    |> Seq.filter (fun (f, _) -> f = frequency)
+    |> Seq.map snd
+    |> Seq.exactlyOne
+    |> Seq.map toCoordinates |> List.ofSeq
+    
+[<Fact>]
+let ``parses map for antennas 0`` () =
+    let expected = [
+        { x = 8; y = 1 }
+        { x = 5; y = 2 }
+        { x = 7; y = 3 }
+        { x = 4; y = 4 }
+    ]
+    
+    let actual = antennaCoordinates testShort "0"
+
+    Assert.Equivalent(expected, actual)
+    Assert.Equal(expected.Length, actual.Length)
+
+[<Fact>]
+let ``parses map for antennas A`` () =
+    let expected = [
+        { x = 6; y = 5 }
+        { x = 8; y = 8 }
+        { x = 9; y = 9 }
+    ]
+    
+    let actual = antennaCoordinates testShort "A"
+
+    Assert.Equivalent(expected, actual)
+    Assert.Equal(expected.Length, actual.Length)
