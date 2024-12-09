384de2c8a57965bfcfe982713b0b3aba56eba5b2
emanuelefirmani <emanuelefirmani@kpmg.com>
2024-12-09 09:31:53 +0100

    D09: Calculate checksum - fails on long test case

---
+++ AdventOfCode/D09/Resolution.fs
@@ -0,0 +1,69 @@
+﻿module AdventOfCode.D09.Resolution
+
+open System
+
+type File = { Id: int; Length: int }
+type Space = { Length: int }
+
+type Item =
+    | File of File
+    | Space of Space
+
+let parseItem i c : Item =
+    let fileId = i / 2
+    let length = Int32.Parse(c.ToString())
+
+    if fileId * 2 = i then
+        File { Id = fileId; Length = length }
+    else
+        Space { Length = length }
+
+let appendIdsToState fileId length state = List.append state (List.map (fun _ -> fileId) [ 1..length ])
+let appendFileToState f state = appendIdsToState f.Id f.Length state
+
+let rec expandList (state: int list) (items: Item list) =
+    match items with
+    | [] -> state
+    | [ head ] ->
+        match head with
+        | File f -> appendFileToState f state
+        | Space _ -> state
+    | head :: rest ->
+        match head with
+        | File f ->
+            let newState = appendFileToState f state
+            expandList newState rest
+        | Space s ->
+            let mutable length = s.Length
+            let mutable newState = state
+            let mutable newItems = rest
+
+            while length > 0 && newItems.Length > 0 do
+                let last = List.last newItems
+                newItems <- List.removeAt (newItems.Length - 1) newItems
+
+                length <-
+                    match last with
+                    | Space _ -> length
+                    | File f ->
+                        if s.Length >= f.Length then
+                            newState <- appendFileToState f newState
+                            length - f.Length
+                        else
+                            newState <- appendIdsToState f.Id length newState
+                            let consumedFile = File { f with Length = f.Length - length }
+                            newItems <- List.append newItems [ consumedFile ]
+                            0
+
+            expandList newState newItems
+
+let parse (input: string) =
+    input.ToCharArray()
+    |> Array.mapi parseItem
+    |> List.ofArray
+    |> expandList []
+
+let calculateChecksum input =
+    parse input
+    |> List.mapi (fun i v -> (i |> decimal) * (v |> decimal))
+    |> List.sum
---
+++ AdventOfCode/D09/Tests.fs
@@ -0,0 +1,40 @@
+﻿module AdventOfCode.D09.Tests
+
+open AdventOfCode.D09.Resolution
+open Xunit
+open System
+open TestCases
+
+let parseToString input =
+    let array =
+        parse input
+        |> List.map _.ToString()
+    String.Join("", array)
+
+
+[<Fact>]
+let ``expands list`` () =
+    let actual = parseToString "12345"
+    Assert.Equal("022111222", actual)
+
+[<Fact>]
+let ``expands list with exact file-to-space match`` () =
+    let actual = parseToString "1533515"
+    Assert.Equal("03333311122222", actual)
+
+[<Fact>]
+let ``expands list 2`` () =
+    let actual = parseToString testShort
+    Assert.Equal("0099811188827773336446555566", actual)
+
+[<Fact>]
+let ``compute checksum short`` () =
+    let actual = calculateChecksum testShort
+    
+    Assert.Equal(1928m, actual)
+
+[<Fact>]
+let ``compute checksum long`` () =
+    let actual = calculateChecksum testLong
+    
+    Assert.Equal(6186204235825m, actual)
