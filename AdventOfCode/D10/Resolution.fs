676ed8dae8e742e4e190bbf3cf48474e883aa660
emanuelefirmani <emanuelefirmani@kpmg.com>
2024-12-11 12:34:12 +0100

    D10: maps is parsed

---
+++ AdventOfCode/D10/Resolution.fs
@@ -0,0 +1,33 @@
+﻿module AdventOfCode.D10.Resolution
+
+open System
+open AdventOfCode
+
+type Coordinates = { x: int; y: int }
+type TrailHead = { coordinates: Coordinates }
+type TrailItem = { coordinates: Coordinates; height: int }
+type TrailSummit = { coordinates: Coordinates }
+
+type Position =
+    | TrailHead of TrailHead
+    | TrailSummit of TrailSummit 
+    | TrailItem of TrailItem
+
+let isHead =
+    function
+    | TrailHead _-> true
+    | _ -> false
+
+let toPosition y x c =
+    let coordinates = { x = x; y = y }
+
+    match c with
+    | '0' -> TrailHead { coordinates = coordinates }
+    | '9' -> TrailSummit { coordinates = coordinates }
+    | _ -> TrailItem { coordinates = coordinates; height = Int32.Parse(c.ToString()) }
+
+let private parseLine y (line: string) =
+    line.ToCharArray()
+    |> Array.mapi (toPosition y)
+
+let parse input = FileSplitter.splitInLines input |> Array.mapi parseLine
---
+++ AdventOfCode/D10/Tests.fs
@@ -0,0 +1,16 @@
+﻿module AdventOfCode.D10.Tests
+
+open AdventOfCode.D10.Resolution
+open TestCases
+open Xunit
+
+[<Fact>]
+let ``compact filesystem`` () =
+    let actual = parse testShort
+    let h = TrailHead { coordinates = { x = 2; y = 0 } }
+    let s = TrailSummit { coordinates = { x = 5; y = 2 } }
+    let i = TrailItem { coordinates = { x = 3; y = 4 }; height = 7 }
+    Assert.Equal(h, actual[0][2])
+    Assert.True(isHead (actual[4][6]))
+    Assert.Equal(s, actual[2][5])
+    Assert.Equal(i, actual[4][3])
