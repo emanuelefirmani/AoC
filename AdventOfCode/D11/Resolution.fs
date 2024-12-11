a09792d8cb23c0a03968d6a942a77e82cd03c254
emanuelefirmani <emanuelefirmani@kpmg.com>
2024-12-12 00:20:29 +0100

    D11: counter uses memoization

--- AdventOfCode/D11/Resolution.fs
+++ AdventOfCode/D11/Resolution.fs
@@ -1,48 +1,43 @@
 ﻿module AdventOfCode.D11.Resolution
 
 open System
+open System.Collections.Generic
+open Microsoft.FSharp.Core
 
-type Stone = { v: decimal; loops: int }
+type Stone = { v: decimal; loop: int }
 
-let blink (stone: Stone) (queue: Stone list) =
+let blink (stone: Stone) =
     if stone.v = 0m then
-        ({ v = 1m; loops = stone.loops + 1 }, queue)
+        [{ v = 1m; loop = stone.loop + 1 }]
     else
         let value = stone.v.ToString()
 
         if value.Length % 2 = 0 then
             let v1 = value[0 .. (value.Length / 2 - 1)]
             let v2 = value[(value.Length / 2) ..]
-            (
-                { v = Decimal.Parse(v1); loops = stone.loops + 1 },
-                { v = Decimal.Parse(v2); loops = stone.loops + 1 } :: queue
-            )
+            [
+             { v = Decimal.Parse(v1); loop = stone.loop + 1 }
+             { v = Decimal.Parse(v2); loop = stone.loop + 1 }
+             ]
         else
-            ({ v = stone.v * 2024m; loops = stone.loops + 1 }, queue)
+            [{ v = stone.v * 2024m; loop = stone.loop + 1 }]
 
-let rec blinkN n (stone: Stone) (queue: Stone list) =
-    if stone.loops = n then
-        queue
-    else
-        let newStone, newQueue = blink stone queue
-        blinkN n newStone newQueue
-
-let afterBlinking n (queue: Stone list) =
-    let mutable i = 0
-    let mutable newQueue = queue
-    while (newQueue.Length > 0) do
-        let head = List.head newQueue
-        let tail =
-            if newQueue.Length > 1 then
-                List.tail newQueue
+let rec afterBlinking (dic: Dictionary<Stone, decimal>) n (stones: Stone seq) =
+    match n with
+    | 0 -> stones |> Seq.length |> decimal
+    | n ->
+        stones
+        |> Seq.map (fun s ->
+            if dic.ContainsKey(s) then
+                dic[s]
             else
-                []
-        newQueue <- blinkN n head tail
-        i <- i + 1
-    i
+                let total = blink s |> afterBlinking dic (n - 1)
+                dic.Add(s, total)
+                total
+            )
+        |> Seq.sum
 
 let parse (input: string) =
     input.Split(' ')
     |> Seq.map decimal
-    |> Seq.map (fun x -> { v = x; loops = 0 })
-    |> List.ofSeq
+    |> Seq.map (fun x -> { v = x; loop = 0 })
--- AdventOfCode/D11/Tests.fs
+++ AdventOfCode/D11/Tests.fs
@@ -1,53 +1,56 @@
 ﻿module AdventOfCode.D11.Tests
 
+open System.Collections.Generic
 open Xunit
 open Resolution
 open TestCases
 
 [<Fact>]
 let ``goes to 1`` () =
-    let s = { v = 0m; loops = 41 }
-    let a, _ = blink s []
-    Assert.Equal(1m, a.v)
-    Assert.Equal(42, a.loops)
+    let s = { v = 0m; loop = 17 }
+    let actual = blink s |> Array.ofSeq
+    Assert.Equal(1m, actual[0].v)
+    Assert.Equal(18, actual[0].loop)
 
 [<Fact>]
 let ``splits`` () =
-    let s = { v = 1241m; loops = 17 }
-    let a, q = blink s []
-    Assert.Equal(12m, a.v)
-    Assert.Equal(41m, q.Head.v)
-    Assert.Equal(18m, q.Head.loops)
+    let s = { v = 1241m; loop = 21 }
+    let actual = blink s |> Array.ofSeq
+    Assert.Equal(12m, actual[0].v)
+    Assert.Equal(22, actual[0].loop)
+    Assert.Equal(41m, actual[1].v)
+    Assert.Equal(22, actual[1].loop)
 
 [<Fact>]
 let ``multiplies`` () =
-    let s = { v = 100m; loops = 12 }
-    let a, _ = blink s []
-    Assert.Equal(202400m, a.v)
+    let s = { v = 100m; loop = 5 }
+    let actual = blink s |> Array.ofSeq
+    Assert.Equal(202400m, actual[0].v)
+    Assert.Equal(6, actual[0].loop)
 
 [<Fact>]
 let ``after blinking`` () =
     let input = parse testShort
-    let actual1 = afterBlinking 1 input
+    let actual1 = afterBlinking (Dictionary<Stone, decimal>()) 1 input
     Assert.Equivalent(3, actual1)
 
-    let actual6 = afterBlinking 6 input
+    let actual6 = afterBlinking (Dictionary<Stone, decimal>()) 6 input
     Assert.Equivalent(22, actual6)
 
 [<Fact>]
 let ``count stones short`` () =
     let input = parse testShort
-    let actual = afterBlinking 25 input
+    let actual = afterBlinking (Dictionary<Stone, decimal>()) 25 input
     Assert.Equivalent(55312m, actual)
 
 [<Fact>]
 let ``count stones long`` () =
     let input = parse testLong
-    let actual = afterBlinking 25 input
+    let actual = afterBlinking (Dictionary<Stone, decimal>()) 25 input
     Assert.Equivalent(217812m, actual)
 
-[<Fact>]
+[<Fact()>]
 let ``count stones long part two`` () =
     let input = parse testLong
-    let actual = afterBlinking 75 input
-    Assert.Equivalent(55312m, actual)
+    let actual = afterBlinking (Dictionary<Stone, decimal>()) 75 input
+    Assert.Equivalent(259112729857522m, actual)
